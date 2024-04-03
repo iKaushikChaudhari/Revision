0eb047b6253554f7e12fdacad544e654b509e13f
Kaushik Chaudhari <100680936+iKaushikChaudhari@users.noreply.github.com>
2024-04-03 12:32:06 +0530

    Create currency.c

---
+++ currency.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main(){
+	int ch1,ch2,c;
+	int samt,ramt;
+	
+	menu:
+		printf("\n=====Menu=====");
+		printf("Enter Choice");
+		printf("\n1. Rupee\n2Dollar\n3Euro");
+		scanf("%d",&ch1);
+		switch(ch1)
+		{
+			case 1:{
+				printf("Enter AMT Rupee ");
+				scanf("%d",&samt);
+				printf("Enter to convert");
+				printf("\n1Dollar\n2Euro");
+				scanf("%d",&ch2);
+				switch()
+				break;
+			}
+		}
+	
+}
