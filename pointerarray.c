d40fe60185eb44741ddb3fbee61fab52f736f81f
Kaushik Chaudhari <100680936+iKaushikChaudhari@users.noreply.github.com>
2024-04-18 13:51:50 +0530

    pointer

---
+++ pointerarray.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main (){
+	int a[100],n,i;
+	int *p;
+	
+	p=&a[i];
+	
+	printf("\nEnter The Number :");
+	scanf("%d",&n);
+	printf("\nEnter Array Elements :");
+	for(i=0;i<n;i++,p++){
+		scanf("%d",p);
+	}
+	p--;
+	printf("\nArray pointer :");
+	for(i=0;i<n;i++,p--){
+		printf(" %d ",*p);
+	}
+}
