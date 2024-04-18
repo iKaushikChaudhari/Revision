ef50dabb8a3e5cebb45db06a5fac9ddababe1325
Kaushik Chaudhari <100680936+iKaushikChaudhari@users.noreply.github.com>
2024-04-18 13:38:02 +0530

    pointr

---
+++ Arraypointer.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main (){
+	int a[100],n,i;
+	int *p;
+	
+	p=&a[i];
+	
+	printf("\nEnter The Number :");
+	scanf("%d",&n);
+	printf("\nEnter Array Elements :");
+	for(i=0;i<n;i++){
+		scanf("%d",&p[i]);
+	}
+	printf("\n Array pointer :");
+	for(i=0;i<n;i++){
+		printf(" %d ",p[i]);
+	}
+}
