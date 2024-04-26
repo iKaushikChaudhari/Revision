82f1c71ca8e4e1c635b942f7eb76d5863f18361b
Kaushik Chaudhari <100680936+iKaushikChaudhari@users.noreply.github.com>
2024-04-26 18:48:30 +0530

    .

---
+++ filehandlingsum.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+int main(){
+	FILE *fp;
+	char data[100];
+	char fname[100];
+	int sum=0;
+	
+	printf("Enter The File Name ");
+	scanf("%s",fname);
+	
+	fp=fopen(fname,"r");
+	while(!feof(fp)){
+		fgets(data,80,fp);
+		sum += atoi(data);
+	}
+	fclose(fp);
+	fp= fopen(fname,"a");
+	
+	fprintf(fp,"\nSum = %d",sum);
+	
+	return 0;
+	
+	
+	
+
+	
+	return 0;
+}
